31346a344d73636c04ee00dc00e19c115f73c5d0
PrajwalBnet <spiderbasnet140@gmail.com>
2024-05-24 08:39:43 +0545

    mission complete

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const rounded = document.querySelector(".rounded");
 const myArea = document.querySelector("#myArea");
+const txtArea = document.querySelector(".txtArea");
+
 console.log(myArea.value);
 console.log("working");
 
 myArea.addEventListener("keyup",(event) =>{
     rounded.style.borderRadius = `${myArea.value}px`;
+    txtArea.textContent = "Border Radius is " + myArea.value;
 })
